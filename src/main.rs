6058f55afd3b8b9e52765dbb0633d1ab5089e9a1
Samy <samy.osmium@gmail.com>
2023-05-22 13:31:11 -0400

    feat: tokenizer (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod preprocessing;
 mod tokenizer;
 
+use tokenizer::get_tokens;
+
 fn main() {
-    println!("Hello, world!");
+    let document = r#"I heard Allah's Messenger (ﷺ) saying, "The reward of deeds depends upon the intentions and every person will get the reward according to what he has intended. So whoever emigrated for worldly benefits or for a woman to marry, his emigration was for what he emigrated for.""#;
+    let tokens = get_tokens(document);
+
+    println!("{:?}", tokens);
 }
