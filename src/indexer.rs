6c2cfb23c87a618d28d7dc256ba8191bd500dbc6
Samy <samy.osmium@gmail.com>
2023-05-22 20:31:47 -0400

    feat: file indexer with multiple file paths

--- src/indexer.rs
+++ src/indexer.rs
@@ -1,15 +1,25 @@
 use std::collections::HashMap;
+use std::fs;
 
 use crate::{
     tokenizer,
     types::{Hadith, HadithCollection},
 };
 
-pub fn get_raw_hadith_file_index(file_path: &str) -> HashMap<String, usize> {
-    let contents =
-        std::fs::read_to_string(file_path).expect("Should have been able to read the file");
+pub fn get_raw_hadith_files_indexes(file_paths: Vec<&str>) -> HashMap<String, usize> {
+    let mut hadiths: Vec<Hadith> = Vec::new();
 
-    let HadithCollection { hadiths } = serde_json::from_str(&contents).expect("this to work");
+    file_paths.iter().for_each(|file_path| {
+        let contents =
+            fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+        let collection: HadithCollection = serde_json::from_str(&contents).expect("this to work");
+
+        collection
+            .hadiths
+            .iter()
+            .for_each(|h| hadiths.push(h.clone()));
+    });
 
     let mut index: HashMap<String, usize> = HashMap::new();
     hadiths.iter().for_each(|Hadith { text }| {
