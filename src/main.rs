85fbe66d90ed7acf8e499e110d1baa3f9b377048
Samy <samy.osmium@gmail.com>
2023-05-22 22:54:33 -0400

    feat: freq term database

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,8 @@ mod term_counter;
 mod tokenizer;
 mod types;
 
-fn main() {}
+fn main() -> Result<(), sqlite::Error> {
+    term_counter::create_and_populate_term_freq_db()?;
+
+    Ok(())
+}
--- src/term_counter.rs
+++ src/term_counter.rs
@@ -1,5 +1,5 @@
+use std::collections::HashMap;
 use std::fs;
-use std::{collections::HashMap, error::Error};
 
 use crate::{
     tokenizer,
@@ -7,9 +7,11 @@ use crate::{
 };
 
 pub fn get_raw_hadith_files_term_counts(file_paths: Vec<&str>) -> HashMap<String, usize> {
+    println!("Getting term counts");
     let mut hadiths: Vec<Hadith> = Vec::new();
 
     file_paths.iter().for_each(|file_path| {
+        println!("Getting term counts for file: {}", file_path);
         let contents =
             fs::read_to_string(file_path).expect("Should have been able to read the file");
 
@@ -32,5 +34,36 @@ pub fn get_raw_hadith_files_term_counts(file_paths: Vec<&str>) -> HashMap<String
         }
     });
 
+    println!("Done getting term counts");
     return term_counts;
+}
+
+pub fn create_and_populate_term_freq_db() -> Result<(), sqlite::Error> {
+    let connection = sqlite::open("./res/db/dev.db")?;
+
+    let file_paths = vec![
+        "./res/raw_hadiths/eng-malik.min.json",
+        "./res/raw_hadiths/eng-bukhari.json",
+        "./res/raw_hadiths/eng-nasai.min.json",
+        "./res/raw_hadiths/eng-muslim.min.json",
+        "./res/raw_hadiths/eng-abudawud.min.json",
+        "./res/raw_hadiths/eng-ibnmajah.min.json",
+        "./res/raw_hadiths/eng-tirmidhi.min.json",
+    ];
+    let term_counts = get_raw_hadith_files_term_counts(file_paths);
+    let insertion_instruction: Vec<String> = term_counts
+        .iter()
+        .map(|(token, count)| format!(r#"('{}', {})"#, token, count))
+        .collect();
+
+    let query = format!(
+        r#"
+    CREATE TABLE term_freqs (token text primary key, freq integer);
+    INSERT INTO term_freqs ("token", "freq") values {};
+    "#,
+        insertion_instruction.join(",")
+    );
+    connection.execute(query)?;
+
+    Ok(())
 }
